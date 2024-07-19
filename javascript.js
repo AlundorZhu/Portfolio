210e90895191001ea24f251e29924ace1d08af46
georgeburin <80087039+georgeburin@users.noreply.github.com>
2024-07-18 19:35:07 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,15 +1,28 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const myelement = document.getElementById('home-id')
+// document.addEventListener("DOMContentLoaded", () => {
+//   let project1 = document.getElementById("project1")
+//   // let project1-picture = documnet.querySelector("project1-picture")
+//
+//   project1.addEventListener("mouseenter", (e) => {
+//     project1.style.opacity = 1;
+//     console.log("hi")
+//   })
+//
+//   project1.addEventListener("mouseleave", (e) => {
+//     project1.style.opacity = 0;
+//     console.log("hi")
+//   })
 
-  const mybutton = document.getElementById("homebutton")
 
 
+  // const mybutton = document.getElementById("homebutton")
 
-  mybutton.addEventListener("click", (e) => {
-    myelement.scrollIntoView({
-        behavior: "smooth"
-    })
-  })
+  // console.log(mybutton)
+  //
+  // mybutton.addEventListener("click", (e) => {
+  //     window.scrollIntoView({
+  //         behavior: "smooth"
+  //     })
+  // })
 });
 
 
